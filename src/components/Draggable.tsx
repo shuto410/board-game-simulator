3bfdd4a2497f08cddef08d63ba96ed8ce0f3adbf
Takumi Shuto <takumi.shuto314@gmail.com>
2022-10-10 02:04:38 +0900

    Test drawer

--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -1,5 +1,7 @@
 import React, { CSSProperties } from 'react';
 import { useDraggable } from '@dnd-kit/core';
+import { useRecoilState } from 'recoil';
+import { drawerVisibilityState } from '../recoil/atoms/ui';
 
 type Props = React.PropsWithChildren & {
   id: string;
@@ -15,6 +17,9 @@ const resetButtonStyles: CSSProperties = {
 };
 
 function Draggable(props: Props) {
+  const [isDrawerVisible, setIsDrawerVisible] = useRecoilState(
+    drawerVisibilityState
+  );
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: props.id,
   });
@@ -26,8 +31,19 @@ function Draggable(props: Props) {
       : undefined,
   };
 
+  const handleOnClick = () => {
+    setIsDrawerVisible(!isDrawerVisible);
+    console.log('handleOnClick');
+  };
+
   return (
-    <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
+    <button
+      ref={setNodeRef}
+      style={style}
+      {...listeners}
+      {...attributes}
+      onClick={handleOnClick}
+    >
       {props.children}
     </button>
   );
